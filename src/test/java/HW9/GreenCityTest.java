374e6a62041535bcfee0d75495c502fd5cedb0e9
Nataliia Khoruzhenko <natelis.nv@gmail.com>
2024-09-24 19:16:08 +0300

    HW11/GreenCityTest

--- src/test/java/HW9/GreenCityTest.java
+++ src/test/java/HW9/GreenCityTest.java
@@ -4,10 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,7 +19,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GreenCityTest {
 
-    // Locators
     @FindBy(css = "app-header:nth-child(1) .ubs-header-sign-in")
     private WebElement signInButton;
 
@@ -31,20 +28,23 @@ public class GreenCityTest {
     @FindBy(css = "app-sign-in .container h2")
     private WebElement signInDetailsText;
 
-    @FindBy(css = "app-sign-in .sign-in-form label[for='email']")
-    private WebElement emailLabel;
+//    @FindBy(css = "app-sign-in .sign-in-form label[for='email']")
+//    private WebElement emailLabel;
 
     @FindBy(css = "app-sign-in .sign-in-form #email")
     private WebElement emailInput;
 
-    @FindBy(css = "app-sign-in .sign-in-form #email-err-msg .app-error")
+    @FindBy(id = "email-err-msg")
     private WebElement emailError;
 
-    @FindBy(css = "app-sign-in .sign-in-form label[for='password']")
-    private WebElement passwordLabel;
+//    @FindBy(css = "app-sign-in .sign-in-form label[for='password']")
+//    private WebElement passwordLabel;
 
     @FindBy(css = "app-sign-in .sign-in-form #password")
     private WebElement passwordInput;
+
+    @FindBy(css = "#pass-err-msg .ng-star-inserted")
+    private WebElement passwordError;
 
     @FindBy(css = "app-sign-in .sign-in-form .ubsStyle")
     private WebElement signInSubmitButton;
@@ -54,12 +54,6 @@ public class GreenCityTest {
 
     @FindBy(css = ".mat-simple-snackbar > span")
     private WebElement result;
-
-//    @FindBy(css = ".alert-general-error.ng-star-inserted")
-//    private WebElement errorMessage;
-
-//    @FindBy(css = "#email-err-msg")
-//    private WebElement errorMessage;
 
     @FindBy(css = "app-header:nth-child(1) .body-2")
     private WebElement usernameElement;
@@ -83,7 +77,8 @@ public class GreenCityTest {
     public void initPageElements() {
         PageFactory.initElements(driver, this);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        // Wait for the language switcher element to appear
+
+        // Wait for the language switcher element and change language if needed
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul[aria-label='language switcher'] li.lang-option > span")));
 
         String currentLanguage = driver.findElement(By.cssSelector("ul[aria-label='language switcher'] li.lang-option > span")).getText();
@@ -107,21 +102,17 @@ public class GreenCityTest {
 
     @ParameterizedTest
     @CsvSource({
-            "xenefa2366@apifan.com, Qwerty1234567!",
-            "cekige2052@klanze.com, Qwerty1!"
+            "xenefa2366@apifan.com, Qwerty1234567!"
     })
     public void signIn(String email, String password) {
         wait.until(ExpectedConditions.elementToBeClickable(signInButton)).click();
 
-        // Wait for the "Welcome back!" text to appear
+        // Wait for the "Welcome back!" text
         wait.until(ExpectedConditions.textToBePresentInElement(welcomeText, "Welcome back!"));
         assertThat(welcomeText.getText().trim(), is("Welcome back!"));
 
         wait.until(ExpectedConditions.visibilityOf(signInDetailsText));
         assertThat(signInDetailsText.getText(), is("Please enter your details to sign in."));
-
-        wait.until(ExpectedConditions.visibilityOf(emailLabel));
-        assertThat(emailLabel.getText(), is("Email"));
 
         emailInput.sendKeys(email);
         assertThat(emailInput.getAttribute("value"), is(email));
@@ -131,15 +122,14 @@ public class GreenCityTest {
 
         wait.until(ExpectedConditions.elementToBeClickable(signInSubmitButton)).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(closeModalButton));
-        closeModalButton.click();
+        // Close modal window
+        wait.until(ExpectedConditions.elementToBeClickable(closeModalButton)).click();
 
-//        wait.until(ExpectedConditions.elementToBeClickable(authModal)).click();
-
+        // Verify sign-in
         wait.until(ExpectedConditions.visibilityOf(usernameElement));
         Assertions.assertEquals("Qwerty1", usernameElement.getText());
 
-        // Sign out process
+        // Sign out
         usernameElement.click();
         wait.until(ExpectedConditions.elementToBeClickable(signOutLink)).click();
 
@@ -150,22 +140,29 @@ public class GreenCityTest {
 
     @ParameterizedTest
     @CsvSource({
-            "Please check if the email is written correctly"
+            "Please check if the email is written correctly",
+//            "Bad email or password"
     })
 
     public void signInNotValid(String expectedErrorMessage) {
         // Click on the Sign In button
         wait.until(ExpectedConditions.elementToBeClickable(signInButton)).click();
+//        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//        jsExecutor.executeScript("arguments[0].click();", signInButton);
 
-        // Enter incorrect email and password
+        // Enter incorrect email
         emailInput.sendKeys("xenefa2366apifan.com");  // Incorrect email format
-        passwordInput.sendKeys("Qwerty1234567!");
 
-        // Click on the Submit button
-        wait.until(ExpectedConditions.elementToBeClickable(signInSubmitButton)).click();
+        // Remove focus from the email input by clicking on the password input field
+        wait.until(ExpectedConditions.elementToBeClickable(passwordInput)).click();
 
-        // Wait for the error message to appear and verify it
-        WebElement errorElement = wait.until(ExpectedConditions.visibilityOf(emailError));
-        assertThat(errorElement.getText().trim(), is(expectedErrorMessage));
+//        // Enter a password
+//        passwordInput.sendKeys("Qwerty1234567!");
+
+        // Wait for the email error message to appear and verify it
+            WebElement errorElement = wait.until(ExpectedConditions.visibilityOf(emailError));
+            assertThat(errorElement.getText().trim(), is(expectedErrorMessage));
     }
-}
+    }
+
+
